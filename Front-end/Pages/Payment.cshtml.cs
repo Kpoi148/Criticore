41015216671da02b6295aab0c312dd3d3da0ede1
lploc75 <lpl070524@gmail.com>
2025-10-09 23:54:39 +0700

    thêm trang thanh toán

---
+++ Front-end/Pages/Payment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Front_end.Pages
+{
+    public class PaymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
