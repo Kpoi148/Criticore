bf54c291785dc0591b3401b5e2db5158ead57499
lploc75 <lpl070524@gmail.com>
2025-10-22 23:10:33 +0700

    thay cột scan id bằng file name, thêm nút xem các nguồn trùng lặp từ internet

---
+++ Front-end/Models/CopyleaksInternetMatch.cs
@@ -0,0 +1,11 @@
+﻿namespace Front_end.Models
+{
+    //lớp nhỏ để deserialize phần internet trong rawResponse:
+    public class CopyleaksInternetMatch
+    {
+        public string Url { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public int MatchedWords { get; set; }
+        public int IdenticalWords { get; set; }
+    }
+}
--- Front-end/Models/CopyleaksReportDto.cs
+++ Front-end/Models/CopyleaksReportDto.cs
@@ -1,4 +1,5 @@
-﻿namespace Front_end.Models
+﻿using Newtonsoft.Json;
+namespace Front_end.Models
 {
     public class CopyleaksReportDto
     {
@@ -11,5 +12,38 @@
         public string ReportUrl { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
         public string? FileName { get; set; }
+        public string? RawResponse { get; set; } // dữ liệu JSON gốc từ Copyleaks
+
+        // List hiển thị nguồn internet (tự parse từ RawResponse)
+        public List<CopyleaksInternetMatch> InternetSources
+        {
+            get
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(RawResponse))
+                        return new List<CopyleaksInternetMatch>();
+
+                    dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(RawResponse)!;
+                    var list = new List<CopyleaksInternetMatch>();
+
+                    foreach (var src in json.results.internet)
+                    {
+                        list.Add(new CopyleaksInternetMatch
+                        {
+                            Url = (string?)src.url ?? "",
+                            Title = (string?)src.title ?? "",
+                            MatchedWords = (int?)src.matchedWords ?? 0,
+                            IdenticalWords = (int?)src.identicalWords ?? 0
+                        });
+                    }
+                    return list;
+                }
+                catch
+                {
+                    return new List<CopyleaksInternetMatch>();
+                }
+            }
+        }
     }
 }
