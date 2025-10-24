c27a25866f21f1131eab93093deac242313a71a9
Khanh_Poi <157507406+KhanhDesignWeb@users.noreply.github.com>
2025-10-24 23:10:09 +0700

    Thêm UI Ai feedback

---
+++ Front-end/Pages/AI/Feedback.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Front_end.Pages.AI
+{
+    public class FeedbackModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
